4177186ebdbfc838653a444da31aec5028fb5b59
Михаил Горбань <onepise333@gmail.com>
2023-03-25 13:40:30 +0300

    Update first.rb
    первая часть первой лабы

--- first_lab/first.rb
+++ first_lab/first.rb
@@ -1,1 +1,23 @@
-asdsda
+puts("Hello world!")
+
+username = ARGV[0]
+ 
+  puts "Как тебя зовут?"
+  puts "Привет, #{username} \nКакой твой любимый язык программирования?"
+  answer = STDIN.gets.chop.downcase
+  case answer
+  when 'ruby'
+    puts "Подлиза!"
+  when 'python'
+    puts "..."
+  when 'c++'
+    puts "Указатели твои эти ну вообще не впечатляют.."
+  else
+    puts 'Скоро будет Ruby!!!!'
+  end
+
+ruby = "puts 'Hello!'"
+eval ruby
+
+cmd = "ls"
+system cmd
