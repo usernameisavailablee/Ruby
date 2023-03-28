efe41e18aa0a1557c92de473a72d5e043ad88bc5
Михаил Горбань <91434795+usernameisavailablee@users.noreply.github.com>
2023-03-29 00:37:11 +0300

    swap

--- first_lab/fourth.rb
+++ first_lab/fourth.rb
@@ -2,3 +2,15 @@
 def local_min? (arr, index)
 	if arr.min  < arr[index] then return false else return true end
 end
+
+def swap_min_max(arr)
+	min_index = arr.index(arr.min)
+	max_index = arr.index(arr.max)
+	buff = arr[min_index]
+	arr[min_index] = arr[max_index]
+	arr[max_index] = buff
+	return arr
+end
+
+
+
