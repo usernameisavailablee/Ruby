2990c74b0e820b27fd956371d0151749b7953bf4
Михаил Горбань <onepise333@gmail.com>
2023-03-29 13:39:05 +0300

    complete full lab
    add interface

--- first_lab/fourth.rb
+++ first_lab/fourth.rb
@@ -1,4 +1,3 @@
-
 def local_min? (arr, index)
 	if arr.min  < arr[index] then return false else return true end
 end
@@ -26,7 +25,58 @@ def new_list (arr)
 	return Array.new(arr.length){|index| index = Random.rand(averange+1..max-1)}
 end
 
-a = [1,-2,3,4,100,-23,122]
-k = new_list(a)
-print k
+puts ("Введите номер метотода где: \n 1 - Проверка на локальный минимум по индексу \n 2 - Поменять минимум и максимум \n 3 - Проверка максимума на интервале (a..b) \n 4 - Сумма модулей структуры \n 5 - Создание нового списка ограниченного средним арифметическим и максимумом")
+
+answer = STDIN.gets.chop.downcase
+  case answer
+  when '1'
+  	puts ('Окей, проверяем. Локален ли минимум?')
+  	puts ("Введите колличество элементов, затем массив")
+  	n = STDIN.gets.to_i
+  	arr = Array.new(n){|index| index = STDIN.gets.to_i}
+
+  	puts ("Введите индекс, для определения локального минимума")
+  	indx = STDIN.gets.to_i
+
+  	puts "Ответ: #{local_min?(arr,indx)}"
+    
+  when '2'
+  	puts ('Окей, меняем местами минимум и максимум')
+    puts ("Введите колличество элементов, затем массив")
+    n = STDIN.gets.to_i
+    arr = Array.new(n){|index| index = STDIN.gets.to_i}
+
+    puts "Итог: #{swap_min_max(arr)}"
+
+  when '3'
+  	puts ('Окей, проверяем. Есть ли максимум на интервале?')
+    puts ("Введите колличество элементов, затем массив")
+    n = STDIN.gets.to_i	
+    arr = Array.new(n){|index| index = STDIN.gets.to_i}
+
+    puts "Введите интервал(два числа, a,b)"
+    a = STDIN.gets.to_i
+    b = STDIN.gets.to_i
+
+    puts "Ответ: #{have_max_in_interval?(arr,a,b)}"
+
+  when '4'
+  	puts ('Окей, сумма модулей структуры.')
+  	puts ("Введите колличество элементов, затем массив")
+    n = STDIN.gets.to_i	
+    arr = Array.new(n){|index| index = STDIN.gets.to_i}
+
+    puts ("Среднее арифметическое модулей: #{average_modules(arr)}")
+
+  when '5'
+  	puts ('Окей, создадим новый список с элементами меньше максимума, но больше среднего арифметического')
+  	puts ("Введите колличество элементов, затем массив")
+    n = STDIN.gets.to_i	
+    arr = Array.new(n){|index| index = STDIN.gets.to_i}
+
+    puts ("Новый список: #{new_list(arr)}")
+
+  else
+    puts ("sad")
+  end
 
