a5c47b5aa0652409daa1865fc4645dc331bcfb57
Михаил Горбань <onepise333@gmail.com>
2023-04-11 02:40:58 +0300

    можно ввести неполноценного пользователя

--- second_lab/classes/student.rb
+++ second_lab/classes/student.rb
@@ -1,7 +1,7 @@
 class Student
-  #присутствует
   attr_accessor :id, :first_name, :last_name, :sur_name,:phone,:tg, :mail, :git
 
+#сразу сделал
   def initialize (id, last_name, first_name, sur_name, phone = "", tg = "", mail = "", git = "")
   	@id = id
     @last_name = last_name
