2f3eca37fa46f5a3c9be7062cc6c6dfb75b5fc45
Михаил Горбань <onepise333@gmail.com>
2023-04-11 14:20:08 +0300

    hash

--- second_lab/classes/student.rb
+++ second_lab/classes/student.rb
@@ -1,22 +1,20 @@
 class Student
   attr_accessor :id, :first_name, :last_name, :sur_name,:phone,:tg, :mail, :git
-
-#сразу сделал
-  def initialize (id, last_name, first_name, sur_name, phone = "", tg = "", mail = "", git = "")
+  def initialize (id, last_name, first_name, sur_name, **contacts)
   	@id = id
     @last_name = last_name
   	@first_name = first_name
   	@sur_name = sur_name
-  	@phone = phone
-  	@tg = tg
-  	@mail = mail
-  	@git = git
+  	@phone = contacts[:phone]
+  	@tg = contacts[:tg]
+  	@mail = contacts[:mail]
+  	@git = contacts[:git]
   end
 
   def print_all()
     conc =  (@id.to_s + " " + @last_name.to_s + " " + 
-      @first_name+ " " + @sur_name + " " + @phone + 
-      " " + @tg + " " + @mail + " " + @git)
+      @first_name.to_s + " " + @sur_name.to_s + " " + @phone.to_s + 
+      " " + @tg.to_s + " " + @mail.to_s + " " + @git.to_s)
     puts conc.split.join(" ")
   end
 end
--- second_lab/main.rb
+++ second_lab/main.rb
@@ -1,9 +1,5 @@
 require './classes/student.rb'
 
-oleg = Student.new(1,'Михляев','Олег','Владимирович','+77777777777','@memeladon','loh@loh.com','memeladon')
-geo = Student.new(2,'Назаров','Георгий','Григорьевич','+88888888888','@tgtgtg','geopox@geo.ua','githzz')
-kresniy_geo = Student.new(3,'Савинян','Шагвел','Парурович')
+bulka = Student.new(1, 'Рупасов', 'Глеб', 'Александрович', tg: '@bagetxxx' , phone:'+7999999999')
 
-oleg.print_all()
-geo.print_all()
-kresniy_geo.print_all()
+bulka.print_all()
