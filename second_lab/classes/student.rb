d54b38c9ca2362a83e967c473df0f9aab7c35fae
Михаил Горбань <91434795+usernameisavailablee@users.noreply.github.com>
2023-04-13 20:40:36 +0300

    &&&&&&&

--- second_lab/classes/student.rb
+++ second_lab/classes/student.rb
@@ -22,10 +22,10 @@ class Student
     self.id = id
     self.last_name = last_name
     self.first_name = first_name
-  	self.sur_name = sur_name
-  	self.tg = tg
-  	self.mail = mail
-  	self.git = git
+    self.sur_name = sur_name
+    self.tg = tg
+    self.mail = mail
+    self.git = git
     self.phone = phone
   end
 
