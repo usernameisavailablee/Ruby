81b7160d44dfa89b294f773706281dce11e4e70a
Михаил Горбань <onepise333@gmail.com>
2023-05-30 16:10:16 +0300

    2.3.8

--- second_lab/main.rb
+++ second_lab/main.rb
@@ -5,38 +5,24 @@ require './classes/data_list.rb'
 require './classes/data_list_student_short.rb'
 
 
-#  str = "id: 123, last_name: Иванов, first_name: Иван, sur_name: Иванович, phone: +71111111111, tg: @asddsa, mail: ivanov@mail.com, git_name: asddsa"
-#  student = Student.from_string(str)
-# # s1 = StudentShort.student_init(student)
-# # a = student.print_all()
-# # print a
+# Создание объектов класса StudentShort
+student1 = StudentShort.new(id: 1, contact_string: "John Doe\tjohndoe\tjohndoe@example.com")
+student2 = StudentShort.new(id: 2, contact_string: "Jane Smith\tjanesmith\tjanesmith@example.com")
+student3 = StudentShort.new(id: 3, contact_string: "Bob Johnson\tbobjohnson\tbobjohnson@example.com")
 
+# Создание списка студентов
+students = [student1, student2, student3]
 
-# Student.write_to_txt('./data_files/students_data'," ")
+# Создание экземпляра DataListStudentShort и передача списка студентов
+data_list = DataListStudentShort.new(students)
 
+# Выделение элементов по номеру
+data_list.select(0)
+data_list.select(2)
 
-#Student.read_from_txt('./data_files/students_data'," ")
-
-
-
-data = [['Значение 1', 'Значение 2'], ['Значение 3', 'Значение 4']]
-table = Data_table.new(data)
-
-value = table.get_element(1, 1)
-puts value  # Вывод: Значение 3
-
-elements = [table]
-
-a =  DataListStudentShort.new(elements)
-p a
-#students = Student.get_students
-#students.each do |student|
-#	puts student.print_all() 
-#end	
-
-
-
-
+# Получение выбранных ID
+selected_ids = data_list.get_selected
+puts "Selected IDs: #{selected_ids}"
 
 
 
