2faec8d83180213f3466341f9cb47b07b187ad4d
Михаил Горбань <onepise333@gmail.com>
2023-07-03 13:32:31 +0300

    потыкался с БД (до 3.1.7 есть )

--- second_lab/DB_working.rb
+++ second_lab/DB_working.rb
@@ -1,21 +1,27 @@
+require './classes/student.rb'
+require './classes/student_short.rb'
+require './classes/data_table.rb'
+require './classes/data_list.rb'
+require './classes/data_list_student_short.rb'
+require './classes/students_list_json.rb'
+require './classes/student_list_yaml.rb'
+require './classes/student_list_super.rb'
+require './classes/studeent_list_strategy.rb'
 require 'sqlite3'
 
 # Устанавливаем соединение с базой данных
 db = SQLite3::Database.new('./data_files/students.db')
 
-# Создаем таблицу
-db.execute <<-SQL
-  CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY,
-    last_name TEXT,
-    first_name TEXT,
-    sur_name TEXT,
-    tg TEXT,
-    mail TEXT,
-    git_name TEXT,
-    phone TEXT
-  );
-SQL
+str = "id: 123, last_name: Иванов, first_name: Иван, sur_name: Иванович, phone: +71111111111, tg: @asddsa, mail: ivanov@mail.com, git_name: asddsa"
+student = Student.from_string(str)
 
+fields = [student.last_name, student.first_name, student.sur_name, student.tg, student.mail, student.git_name, student.phone]
+
+
+db.execute('insert into students (last_name, first_name, sur_name, tg, mail, git_name, phone) VALUES (?, ?, ?, ?, ?, ?, ?)', *fields)
+
+results = db.execute("SELECT * FROM students")
+
+puts results
 # Закрываем соединение с базой данных
 db.close
